bf2ce39710d9328536b459266cd8a030e06ac5c9
prajwalsundas47-lang <prajwalsundas47@gmail.com>
2026-08-21 20:25:52 +0545

    Add listener registration and notification functions

---
+++ bb_notify.py
@@ -0,0 +1,11 @@
+_listeners = []
+
+def register(cb):
+    _listeners.append(cb)
+
+def notify(msg):
+    for cb in list(_listeners):
+        try:
+            cb(msg)
+        except Exception:
+            pass
