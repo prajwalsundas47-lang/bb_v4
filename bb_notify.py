cdd64dd15ac618cbe3e377df459bae949135827a
prajwalsundas47-lang <prajwalsundas47@gmail.com>
2026-08-26 19:36:14 +0545

    Remove unused BB notify callback function

--- bb_notify.py
+++ bb_notify.py
@@ -10,7 +10,4 @@ def notify(msg):
         except Exception:
             pass
 
-def _on_bb_notify(msg):
-    self.add_message("BB", msg)   # or however you append to chat_log
 
-bb_notify.register(_on_bb_notify)
