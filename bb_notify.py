a5bad1f74f043540a98deaf8a6dc9fdcd91d475c
prajwalsundas47-lang <prajwalsundas47@gmail.com>
2026-08-26 19:18:26 +0545

    Add callback for BB notifications
    Register a callback function to handle BB notifications.

--- bb_notify.py
+++ bb_notify.py
@@ -9,3 +9,8 @@ def notify(msg):
             cb(msg)
         except Exception:
             pass
+
+def _on_bb_notify(msg):
+    self.add_message("BB", msg)   # or however you append to chat_log
+
+bb_notify.register(_on_bb_notify)
